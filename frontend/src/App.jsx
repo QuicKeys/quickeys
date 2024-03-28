e340e7061880dd06440732c248219ba0523d5753
Maburiku <mjrpigao@addu.edu.ph>
2024-03-28 19:31:07 +0800

    Install THREE.js, GLTFLoader, & Framer Motion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { motion } from "framer-motion"
+
 function App() {
   const [count, setCount] = useState(0)
 
