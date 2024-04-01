cc324850e03a804f200fb01a500868022134afda
adriannebulao <amcbulao@gmail.com>
2024-04-01 11:53:29 +0800

    Add user address profiling API endpoints

--- backend/user_profiling/schemas/user_profile_schemas.py
+++ backend/user_profiling/schemas/user_profile_schemas.py
@@ -1,13 +1,29 @@
 from ninja import Schema
 from datetime import date, datetime
 
+
+class AuthUserOut(Schema):
+    username: str
+    first_name: str
+    last_name: str
+    email: str
+    date_joined: datetime
+
 class UserIn(Schema):
     auth_user_id: int
     birthdate: date
     contact_no: str
 
 class UserOut(Schema):
-    auth_user_id: int
+    auth_user: AuthUserOut
     birthdate: date
     contact_no: str
+    created_at: datetime
+
+class UserAddressIn(Schema):
+    user_address: str
+
+class UserAddressOut(Schema):
+    user_id: int 
+    user_address: str
     created_at: datetime
