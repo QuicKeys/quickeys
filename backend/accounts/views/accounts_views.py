839a667ccf7cff257aac382a3cca1851c0288a01
adriannebulao <amcbulao@gmail.com>
2024-04-05 02:19:19 +0800

    feat: add access and refresh token functionality

--- backend/accounts/views/accounts_views.py
+++ backend/accounts/views/accounts_views.py
@@ -7,6 +7,7 @@ from ..serializers.accounts_serializers import (
     UserSignUpSerializer,
     UserLogInSerializer
 )
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class SignUpView(APIView):
@@ -34,8 +35,13 @@ class LogInView(APIView):
             account = authenticate(request, username=request.data['username'], password=request.data['password'])
             if account is not None:
                 login(request, account)
+
                 # Generate token
-                return Response({'token': 'your-auth-token'})
+                refresh = RefreshToken.for_user(account)
+                access_token = str(refresh.access_token)
+                refresh_token = str(refresh)
+
+                return Response({'access_token': access_token, 'refresh_token': refresh_token})
             else:
                 return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         else:
