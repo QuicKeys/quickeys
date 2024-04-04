76b684d949e18c50173d2fb6a822aaba61d25b8d
adriannebulao <amcbulao@gmail.com>
2024-04-04 23:37:44 +0800

    Update accounts app serializers

--- backend/accounts/views/accounts_views.py
+++ backend/accounts/views/accounts_views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
-from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
