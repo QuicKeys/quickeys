81de458561e174425b8dc1ec049e9fc041302daa
adriannebulao <amcbulao@gmail.com>
2024-04-06 17:13:28 +0800

    feat: add user address API endpoints

--- backend/user_profiling/serializers/user_address_serializers.py
+++ backend/user_profiling/serializers/user_address_serializers.py
@@ -6,14 +6,14 @@ from .user_profile_serializers import UserProfileOutSerializer
 class UserAddressInSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAddress
-        fields = ['user_address']
+        fields = ['user', 'user_address']
 
 class UserAddressOutSerializer(serializers.ModelSerializer):
     user = UserProfileOutSerializer(read_only=True)
 
     class Meta:
         model = UserAddress
-        fields = ['user', 'user_address', 'created_at']
+        fields = ['user_address_id', 'user', 'user_address', 'created_at']
 
 class UserAddressListOutSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/user_profiling/urls.py
+++ backend/user_profiling/urls.py
@@ -4,6 +4,11 @@ from .views.user_profile_views import (
     UserProfileDetailAPIView,
     UserProfileListAPIView
 )
+from .views.user_address_views import (
+    UserAddressCreateAPIView,
+    UserAddressDetailAPIView,
+    UserAddressListAPIView
+)
 
 
 urlpatterns = [
@@ -13,4 +18,11 @@ urlpatterns = [
     path('profile/edit/<int:user_id>/', UserProfileDetailAPIView.as_view(), name='user-profile-edit'),
     path('profile/delete/<int:user_id>/', UserProfileDetailAPIView.as_view(), name='user-profile-delete'),
     path('profile/list/', UserProfileListAPIView.as_view(), name='user-profile-list'),
+
+    #User Address API Endpoint URLs
+    path('profile/address/create/', UserAddressCreateAPIView.as_view(), name='user-address-create'),
+    path('profile/<int:user_id>/address/view/<int:user_address_id>/', UserAddressDetailAPIView.as_view(), name='user-address-detail'),
+    path('profile/<int:user_id>/address/edit/<int:user_address_id>/', UserAddressDetailAPIView.as_view(), name='user-address-edit'),
+    path('profile/<int:user_id>/address/delete/<int:user_address_id>/', UserAddressDetailAPIView.as_view(), name='user-address-delete'),
+    path('profile/<int:user_id>/address/list/', UserAddressListAPIView.as_view(), name='user-address-list'),
 ]
