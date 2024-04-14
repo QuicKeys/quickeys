810cb160b2126c9f7eb11b44a51e80b72799688f
Maburiku <mjrpigao@addu.edu.ph>
2024-04-15 00:20:58 +0800

    Shop Card

--- frontend/client/src/components/pages/Contact.jsx
+++ frontend/client/src/components/pages/Contact.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Reveal } from '../Reveal'
 
 function Contact() {
     return (
       <>
         <div className="px-[50px] py-[100px]">
-          <div className="flex justify-center text-[75px] font-medium"> <span className="text-[#00FF8A] pr-[20px]">CONTACT US</span> PAGE </div>
-          <div className="flex justify-center">[ work in progress ]</div>
+          <Reveal>
+            <div className="flex justify-center text-[75px] font-medium"> <span className="text-[#00FF8A] pr-[20px]">CONTACT US</span> PAGE </div>
+          </Reveal>
+          <Reveal>
+            <div className="flex justify-center">[ work in progress ]</div>
+          </Reveal>
         </div>
       </>
     )
--- frontend/client/src/components/pages/Shop.jsx
+++ frontend/client/src/components/pages/Shop.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
+import { Reveal } from '../Reveal'
 
 function Shop() {
     return (
       <>
         <div className="px-[50px] py-[100px]">
-          <div className="flex justify-center text-[75px] font-medium"> <span className="text-[#00FF8A] pr-[20px]">SHOP</span> PAGE </div>
-          <div className="flex justify-center">[ work in progress ]</div>
+          <Reveal>
+            <div className="flex justify-center text-[75px] font-medium"> <span className="text-[#00FF8A] pr-[20px]">SHOP</span> PAGE </div>
+          </Reveal>
+          <Reveal>
+            <div className="flex justify-center">[ work in progress ]</div>
+          </Reveal>
 
           <div className="flex justify-center w-full">
-            <div className="grid grid-cols-3 gap-[75px]">
+            <div className="grid grid-cols-3 gap-[25px]">
 
-              <div className="flex justify-center w-full">
-                
-              </div>
+                {/* ITEM */}
+                <Reveal>
+                  <div className="flex flex-col h-[400px] w-[300px]">
+                    <div className="ItemCard-Image" alt="Item Image">
+                      [ Missing Item Image. ]
+                    </div>
+                    <p className="ItemCard-Name" alt="Item Name">QuicKeys™ Master Keyboard Building Kit V2 Blue</p>
+                    <p className="ItemCard-Brand" alt="Item Brand">QuicKeys™ Keyboards</p>
+                    <p className="ItemCard-Price" alt="Item Price">₱2,195</p>
+                  </div>
+                </Reveal>
 
             </div>
           </div>
