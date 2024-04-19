a466654c6c000be02c3f1b2314d882b8784ca4b1
Maburiku <mjrpigao@addu.edu.ph>
2024-04-19 23:38:26 +0800

    Responsive Shop Cards Design

--- frontend/client/src/components/pages/Shop.jsx
+++ frontend/client/src/components/pages/Shop.jsx
@@ -35,37 +35,44 @@ function Shop() {
     return (
       <>
         <div className="px-[50px] py-[100px]">
-          <Reveal>
+          {/* <Reveal>
             <div className="flex justify-center text-[75px] font-medium"> <span className="text-[#00FF8A] pr-[20px]">SHOP</span> PAGE </div>
           </Reveal>
           <Reveal>
             <div className="flex justify-center">[ work in progress ]</div>
-          </Reveal>
+          </Reveal> */}
 
-          <div className="flex justify-center w-full">
-            <div className="grid grid-cols-3 gap-[25px]">
+          {/* <Reveal>
+            <section className="flex justify-center w-full">
+              <img className="max-w-[1700px]" src="./src/assets/QuicKeys WORDMARK.svg"/>
+            </section>
+          </Reveal> */}
+
+          <div className="flex flex-col items-center">
+            <div className="grid grid-cols-1 nm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-[45px] relative">
               {items.map(item => (
-              <Reveal key={item.item_id}>
-                <div className="group flex flex-col h-[400px] w-[300px]">
-                  <div className="ItemCard-Image" alt={`${item.item_name} image`}>
-                    <img
-                      className="transition-all duration-500 group-hover:scale-110"
-                      src={item.item_profile_picture_link}>
-                    </img>
+                <Reveal key={item.item_id}>
+                  <div className="group flex flex-col h-[400px] w-[300px]">
+                    <div className="ItemCard-Image" alt={`${item.item_name} image`}>
+                      <img
+                        className="transition-all duration-500 p-[25px] group-hover:scale-105"
+                        src={item.item_profile_picture_link}>
+                      </img>
+                    </div>
+                    <p className="ItemCard-Name group-hover:underline" alt={`${item.item_name}`}>{item.item_name}</p>
+                    <p className="ItemCard-Brand" alt={`${item.item_brand.item_brand_name}`}>{item.item_brand.item_brand_name}</p>
+                    <p className="ItemCard-Price" alt={`${item.item_price}`}>₱{parseFloat(item.item_price).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
                   </div>
-                  <p className="ItemCard-Name group-hover:underline" alt={`${item.item_name}`}>{item.item_name}</p>
-                  <p className="ItemCard-Brand" alt={`${item.item_brand.item_brand_name}`}>{item.item_brand.item_brand_name}</p>
-                  <p className="ItemCard-Price" alt={`${item.item_price}`}>₱{parseFloat(item.item_price).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                </div>
-            </Reveal>
+                </Reveal>
               ))}
+              <div className="absolute top-0 left-0 transform">
+                <p>test</p>
+              </div>
             </div>
           </div>
-            <Reveal>
-              {nextPage && (
-                <button onClick={fetchNextPage}>Load More</button>
-              )}
-            </Reveal>
+          <Reveal>
+            {nextPage && (<button onClick={fetchNextPage}>Load More</button>)}
+          </Reveal>
         </div>
       </>
     )
